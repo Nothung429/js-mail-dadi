680006bdc0a5c59af82873f2ed21f313e9acb5a3
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-03 17:01:42 +0200

    aggiornamento gioco dei dadi

--- Gioco dadi/js/script.js
+++ Gioco dadi/js/script.js
@@ -1,6 +1,24 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-const player = Math.floor(Math.random() * 6) + 1;
-console.log(player);
-const computer = Math.floor(Math.random() * 6) + 1;
-console.log(computer);
+const result = document.getElementById("result")
+const btnPlay = document.getElementById("play")
+btnPlay.addEventListener("click",
+    function(){
+        const playerDice = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("player").innerHTML = `Player point: ${playerDice}`
+        const computerDice = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("computer").innerHTML = `Computer point: ${computerDice}`
+        console.log(player);
+        console.log(computer);
+        if (playerDice > computerDice) {
+            result.style.display = "block";
+            result.innerHTML = `HAI VINTO!!!`
+        } else if (playerDice < computerDice) {
+            result.style.display = "block";
+            result.innerHTML = `HAI PERSO`
+        } else {
+            result.style.display = "block";
+            result.innerHTML = `PAREGGIO`
+        }
+    }
+);
