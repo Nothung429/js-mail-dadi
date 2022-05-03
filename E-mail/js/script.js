152752ffecd0d8ba250b6f20a2eb426ec518c04a
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-03 17:07:13 +0200

    aggiunta richiesta mail

--- E-mail/js/script.js
+++ E-mail/js/script.js
@@ -1,3 +1,5 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+const emailRequest = prompt("inserisci la tua e-mail");
+console.log(emailRequest);
